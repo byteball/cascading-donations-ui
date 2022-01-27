9c07bf9e5febaa58e21632573cc2e81f29b18dc3
tony <tonyofbyteball@users.noreply.github.com>
2022-01-27 19:19:48 +0300

    add repo intro

--- src/pages/AddPage/steps/WalletStep.tsx
+++ src/pages/AddPage/steps/WalletStep.tsx
@@ -2,7 +2,8 @@ import { AddWalletModal } from "modals"
 
 export const WalletStep: React.FC = () => (
   <div>
-    <p>Please add your wallet to the site.</p>
+    <p>Your repo can receive donations already now, without any setup. However, to set up your own distribution rules and withdraw the donated money, you need to prove ownership of the repo and link it to your wallet.</p>
+    <p>As the first step, please add your wallet to the site.</p>
     <div>
       <AddWalletModal triggerButtonIsPrimary={true} />
     </div>
